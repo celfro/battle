0906f02ccfa87fbea93a105de0179258b0b444c5
Alexis Josephides <ajosephides@gmail.com>
2019-06-13 16:48:17 +0100

    added player 2 hit points

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class Battle < Sinatra::Base
   get '/play' do
     @player_1_name = session[:player_1_name]
     @player_2_name = session[:player_2_name]
-
+    @player_2_points = 50
     erb(:play)
   end
 
